70ab32bbb034fafd7df60a535eac5bf40aadb72e
lijinfeng <lijinfeng@yuxirj.com>
2026-03-06 14:32:55 +0800

    add code-inspector-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,24 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [    
+    codeInspectorPlugin({
+      bundler: 'vite',
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 })
